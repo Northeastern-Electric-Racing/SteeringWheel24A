e77a8b492d4a724a3a9b3fb19477cefede64be1c
bjackson312006 <blakejackson312006@gmail.com>
2025-01-26 13:14:05 -0500

    Set Torque Dial pins to pull-down

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -308,7 +308,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
                           |GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
